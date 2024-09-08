8fe6358cae9916d3f59f9f43088d27e56fe762ad
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-09-09 01:52:42 +0530

    Add Pilots Report Functionality

--- src/api/controllers/report.controller.js
+++ src/api/controllers/report.controller.js
@@ -2,6 +2,7 @@ const logger = require('../../utils/logger');
 const ErrorResponse = require('../../utils/ErrorResponse');
 const puppeteer = require('puppeteer');
 const AirfieldSchema = require('../models/Airfield');
+const PilotSchema = require('../models/Pilot');
 const os = require('os');
 const path = require('path');
 
@@ -317,7 +318,330 @@ const generateAllAirfieldsPdf = async (req, res) => {
     }
 }
 
+const generateAllPilotsPdf = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const pilots = await PilotSchema.find({ userId: userId });
+
+        const tableRows = pilots.map((item, index) => `
+            <div class="tbody-record">
+                <div class="tbody-data">${index+1}</div>
+                <div class="tbody-data">${item.fname} ${item.lname}</div>
+                <div class="tbody-data">${ 
+                    item.email === undefined ? 'N/A' : item.email
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.mobile === undefined ? 'N/A' : item.mobile
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.employee_id === undefined ? 'N/A' : item.employee_id
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.company === undefined ? 'N/A' : item.company
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.position === undefined ? 'N/A' : item.position
+                 }</div>
+            </div>
+        `).join('');
+
+        // Set the HTML content directly
+        const htmlContent = `
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                        text-decoration: none;
+                    }
+            
+                    .body {
+                        padding: 20px 50px;
+                        background-color: white;
+                    }
+            
+                    .body .table {
+                        width: 100%;
+                        min-height: 100vh;
+                        border-radius: 0.25rem;
+                    }
+            
+                    .body .table .table-header {
+                        width: 100%;
+                        background-color: #e4e4e4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        padding: 10px;
+                        border: 2px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-header .theader-data {
+                        align-items: center;
+                        justify-content: center;
+                        display: flex;
+                        font-family: Arial, Helvetica, sans-serif;
+                        font-size: 14px;
+                    }
+            
+                    .body .table .table-body {
+                        width: 100%;
+                    }
+            
+                    .body .table .table-body .tbody-record {
+                        width: 100%;
+                        background-color: #f4f4f4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        border-left: 1px solid rgb(189, 189, 189);
+                        border-right: 1px solid rgb(189, 189, 189);
+                        border-bottom: 1px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-body .tbody-record .tbody-data {
+                        background-color: #f4f4f4;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        font-size: 10px;
+                        padding: 10px 0;
+                        font-weight: normal;
+                        text-align: center;
+                        font-family: Arial, Helvetica, sans-serif;
+                    }
+                </style>
+            </head>
+            <body class="body">
+                <div class="table">
+                    <div class="table-header">
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">No.</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Name</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Email</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Mobile</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Employee ID</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Company</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Position</div>
+                    </div>
+                    <div class="table-body">
+                        ${tableRows}
+                    </div>
+                </div>
+            </body>
+            </html>
+        `;
+
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        await page.setContent(htmlContent);
+
+        const date = new Date();
+        const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        // Generate the PDF
+        const pdfBuffer = await page.pdf({ path: `${desktopPath}/AllPilots ${formattedDate}.pdf`, format: 'A4' });
+
+        await browser.close();
+
+        // Set the response headers for PDF download
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="asdfasdf.pdf"',
+            'Content-Length': pdfBuffer.length
+        });
+
+        // Send the PDF buffer as the response
+        res.send(pdfBuffer);
+
+        logger.info("PDF Created Successfully");
+        res.status(201).send("Generate PDF query successfull");
+
+    } catch (error) {
+        // Log the error before sending any response
+        logger.error("Error generating PDF:", error.message);
+
+        // If the stream hasn't started, send an error response
+        if (!res.headersSent) {
+            res.status(500).json(
+                new ErrorResponse(
+                    500,
+                    "Generate PDF query failed",
+                    error.message
+                )
+            );
+        } else {
+            logger.error("Stream error after headers sent, cannot send JSON response.");
+        }
+    }
+}
+
+const generateSelectedPilotsPdf = async (req, res) => {
+    try {
+        const { pilotsList } = req.body;
+
+        const tableRows = pilotsList.map((item, index) => `
+            <div class="tbody-record">
+                <div class="tbody-data">${index+1}</div>
+                <div class="tbody-data">${item.fname} ${item.lname}</div>
+                <div class="tbody-data">${ 
+                    item.email === undefined ? 'N/A' : item.email
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.mobile === undefined ? 'N/A' : item.mobile
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.employee_id === undefined ? 'N/A' : item.employee_id
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.company === undefined ? 'N/A' : item.company
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.position === undefined ? 'N/A' : item.position
+                 }</div>
+            </div>
+        `).join('');
+
+        console.log(tableRows);
+
+        // Set the HTML content directly
+        const htmlContent = `
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                        text-decoration: none;
+                    }
+            
+                    .body {
+                        padding: 20px 50px;
+                        background-color: white;
+                    }
+            
+                    .body .table {
+                        width: 100%;
+                        min-height: 100vh;
+                        border-radius: 0.25rem;
+                    }
+            
+                    .body .table .table-header {
+                        width: 100%;
+                        background-color: #e4e4e4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        padding: 10px;
+                        border: 2px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-header .theader-data {
+                        align-items: center;
+                        justify-content: center;
+                        display: flex;
+                        font-family: Arial, Helvetica, sans-serif;
+                        font-size: 14px;
+                    }
+            
+                    .body .table .table-body {
+                        width: 100%;
+                    }
+            
+                    .body .table .table-body .tbody-record {
+                        width: 100%;
+                        background-color: #f4f4f4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        border-left: 1px solid rgb(189, 189, 189);
+                        border-right: 1px solid rgb(189, 189, 189);
+                        border-bottom: 1px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-body .tbody-record .tbody-data {
+                        background-color: #f4f4f4;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        font-size: 10px;
+                        padding: 10px 0;
+                        font-weight: normal;
+                        text-align: center;
+                        font-family: Arial, Helvetica, sans-serif;
+                    }
+                </style>
+            </head>
+            <body class="body">
+                <div class="table">
+                    <div class="table-header">
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">No.</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Name</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Email</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Mobile</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Employee ID</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Company</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Position</div>
+                    </div>
+                    <div class="table-body">
+                        ${tableRows}
+                    </div>
+                </div>
+            </body>
+            </html>
+        `;
+
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        await page.setContent(htmlContent);
+
+        const date = new Date();
+        const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        // Generate the PDF
+        const pdfBuffer = await page.pdf({ path: `${desktopPath}/SelectedPilots ${formattedDate}.pdf`, format: 'A4' });
+
+        await browser.close();
+
+        // Set the response headers for PDF download
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="asdfasdf.pdf"',
+            'Content-Length': pdfBuffer.length
+        });
+
+        // Send the PDF buffer as the response
+        res.send(pdfBuffer);
+
+        logger.info("PDF Created Successfully");
+        res.status(201).send("Generate PDF query successfull");
+
+    } catch (error) {
+        // Log the error before sending any response
+        logger.error("Error generating PDF:", error.message);
+
+        // If the stream hasn't started, send an error response
+        if (!res.headersSent) {
+            res.status(500).json(
+                new ErrorResponse(
+                    500,
+                    "Generate PDF query failed",
+                    error.message
+                )
+            );
+        } else {
+            logger.error("Stream error after headers sent, cannot send JSON response.");
+        }
+    }
+}
+
 module.exports = {
     generateAllAirfieldsPdf,
-    generateSelectedAirfieldsPdf
+    generateSelectedAirfieldsPdf,
+    generateAllPilotsPdf,
+    generateSelectedPilotsPdf
 }
--- src/api/routes/report.route.js
+++ src/api/routes/report.route.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { generateAllAirfieldsPdf, generateSelectedAirfieldsPdf } = require('../controllers/report.controller');
+const { generateAllAirfieldsPdf, generateSelectedAirfieldsPdf, generateAllPilotsPdf, generateSelectedPilotsPdf } = require('../controllers/report.controller');
 
 const router = express.Router();
 
-//generate pdf
+//generate airfields pdf
 router.post('/generateAllAirfieldsPdf', generateAllAirfieldsPdf);
 router.post('/generateSelectedAirfieldsPdf', generateSelectedAirfieldsPdf);
+
+//generate pilots pdf
+router.post('/generateAllPilotsPdf', generateAllPilotsPdf);
+router.post('/generateSelectedPilotsPdf', generateSelectedPilotsPdf);
 
 module.exports = router;
