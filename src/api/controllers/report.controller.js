7b55e8b9a0270ecea0bd9fa7cde5d06a412685a9
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2024-09-09 03:45:58 +0530

    Add Flights Report Functionality

--- src/api/controllers/report.controller.js
+++ src/api/controllers/report.controller.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 const AirfieldSchema = require('../models/Airfield');
 const PilotSchema = require('../models/Pilot');
 const AircraftSchema = require('../models/Aircraft');
+const FlightSchema = require('../models/Flight');
 const os = require('os');
 const path = require('path');
 
@@ -959,11 +960,337 @@ const generateSelectedAircraftsPdf = async (req, res) => {
     }
 }
 
+const generateAllFlightsPdf = async (req, res) => {
+    try {
+        const { userId } = req.body;
+        const flights = await FlightSchema.find({ userId: userId })
+            .populate('departure')
+            .populate('arrival')
+            .populate('aircraft')
+            .exec();
+
+        const tableRows = flights.map((item, index) => `
+            <div class="tbody-record">
+                <div class="tbody-data">${index+1}</div>
+                <div class="tbody-data">${item.date.toUTCString()}</div>
+                <div class="tbody-data">${ 
+                    item.aircraft.registration_no === undefined ? 'N/A' : item.aircraft.registration_no
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.arrival.airport_name === undefined ? 'N/A' : item.arrival.airport_name
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.departure.airport_name === undefined ? 'N/A' : item.departure.airport_name
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.in_time === undefined ? 'N/A' : item.in_time
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.out_time === undefined ? 'N/A' : item.out_time
+                 }</div>
+            </div>
+        `).join('');
+
+        // Set the HTML content directly
+        const htmlContent = `
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                        text-decoration: none;
+                    }
+            
+                    .body {
+                        padding: 20px 50px;
+                        background-color: white;
+                    }
+            
+                    .body .table {
+                        width: 100%;
+                        min-height: 100vh;
+                        border-radius: 0.25rem;
+                    }
+            
+                    .body .table .table-header {
+                        width: 100%;
+                        background-color: #e4e4e4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        padding: 10px;
+                        border: 2px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-header .theader-data {
+                        align-items: center;
+                        justify-content: center;
+                        display: flex;
+                        font-family: Arial, Helvetica, sans-serif;
+                        font-size: 14px;
+                    }
+            
+                    .body .table .table-body {
+                        width: 100%;
+                    }
+            
+                    .body .table .table-body .tbody-record {
+                        width: 100%;
+                        background-color: #f4f4f4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        border-left: 1px solid rgb(189, 189, 189);
+                        border-right: 1px solid rgb(189, 189, 189);
+                        border-bottom: 1px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-body .tbody-record .tbody-data {
+                        background-color: #f4f4f4;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        font-size: 10px;
+                        padding: 10px 0;
+                        font-weight: normal;
+                        text-align: center;
+                        font-family: Arial, Helvetica, sans-serif;
+                    }
+                </style>
+            </head>
+            <body class="body">
+                <div class="table">
+                    <div class="table-header">
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">No.</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Date</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Flight No.</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Arrival</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Departure</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">In</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Out</div>
+                    </div>
+                    <div class="table-body">
+                        ${tableRows}
+                    </div>
+                </div>
+            </body>
+            </html>
+        `;
+
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        await page.setContent(htmlContent);
+
+        const date = new Date();
+        const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        // Generate the PDF
+        const pdfBuffer = await page.pdf({ path: `${desktopPath}/AllFlights ${formattedDate}.pdf`, format: 'A4' });
+
+        await browser.close();
+
+        // Set the response headers for PDF download
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="asdfasdf.pdf"',
+            'Content-Length': pdfBuffer.length
+        });
+
+        // Send the PDF buffer as the response
+        res.send(pdfBuffer);
+
+        logger.info("PDF Created Successfully");
+        res.status(201).send("Generate PDF query successfull");
+
+    } catch (error) {
+        // Log the error before sending any response
+        logger.error("Error generating PDF:", error.message);
+
+        // If the stream hasn't started, send an error response
+        if (!res.headersSent) {
+            res.status(500).json(
+                new ErrorResponse(
+                    500,
+                    "Generate PDF query failed",
+                    error.message
+                )
+            );
+        } else {
+            logger.error("Stream error after headers sent, cannot send JSON response.");
+        }
+    }
+}
+
+const generateSelectedFlightsPdf = async (req, res) => {
+    try {
+        const { flightsList } = req.body;
+        console.log(flightsList);
+
+        const tableRows = flightsList.map((item, index) => `
+            <div class="tbody-record">
+                <div class="tbody-data">${index+1}</div>
+                <div class="tbody-data">${item.date}</div>
+                <div class="tbody-data">${ 
+                    item.aircraft.registration_no === undefined ? 'N/A' : item.aircraft.registration_no
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.arrival.airport_name === undefined ? 'N/A' : item.arrival.airport_name
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.departure.airport_name === undefined ? 'N/A' : item.departure.airport_name
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.in_time === undefined ? 'N/A' : item.in_time
+                 }</div>
+                <div class="tbody-data">${ 
+                    item.out_time === undefined ? 'N/A' : item.out_time
+                 }</div>
+            </div>
+        `).join('');
+
+        // Set the HTML content directly
+        const htmlContent = `
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>Document</title>
+                <style>
+                    * {
+                        margin: 0;
+                        padding: 0;
+                        box-sizing: border-box;
+                        text-decoration: none;
+                    }
+            
+                    .body {
+                        padding: 20px 50px;
+                        background-color: white;
+                    }
+            
+                    .body .table {
+                        width: 100%;
+                        min-height: 100vh;
+                        border-radius: 0.25rem;
+                    }
+            
+                    .body .table .table-header {
+                        width: 100%;
+                        background-color: #e4e4e4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        padding: 10px;
+                        border: 2px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-header .theader-data {
+                        align-items: center;
+                        justify-content: center;
+                        display: flex;
+                        font-family: Arial, Helvetica, sans-serif;
+                        font-size: 14px;
+                    }
+            
+                    .body .table .table-body {
+                        width: 100%;
+                    }
+            
+                    .body .table .table-body .tbody-record {
+                        width: 100%;
+                        background-color: #f4f4f4;
+                        display: grid;
+                        grid-template-columns: repeat(7, 1fr);
+                        border-left: 1px solid rgb(189, 189, 189);
+                        border-right: 1px solid rgb(189, 189, 189);
+                        border-bottom: 1px solid rgb(189, 189, 189);
+                    }
+            
+                    .body .table .table-body .tbody-record .tbody-data {
+                        background-color: #f4f4f4;
+                        display: flex;
+                        align-items: center;
+                        justify-content: center;
+                        font-size: 10px;
+                        padding: 10px 0;
+                        font-weight: normal;
+                        text-align: center;
+                        font-family: Arial, Helvetica, sans-serif;
+                    }
+                </style>
+            </head>
+            <body class="body">
+                <div class="table">
+                    <div class="table-header">
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">No.</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Date</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Flight No.</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Arrival</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Departure</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">In</div>
+                        <div class="theader-data" style="border-right: 2px solid rgb(189, 189, 189);">Out</div>
+                    </div>
+                    <div class="table-body">
+                        ${tableRows}
+                    </div>
+                </div>
+            </body>
+            </html>
+        `;
+
+        const browser = await puppeteer.launch({ headless: true });
+        const page = await browser.newPage();
+
+        await page.setContent(htmlContent);
+
+        const date = new Date();
+        const formattedDate = `${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`;
+        // Generate the PDF
+        const pdfBuffer = await page.pdf({ path: `${desktopPath}/SelectedFlights ${formattedDate}.pdf`, format: 'A4' });
+
+        await browser.close();
+
+        // Set the response headers for PDF download
+        res.set({
+            'Content-Type': 'application/pdf',
+            'Content-Disposition': 'attachment; filename="asdfasdf.pdf"',
+            'Content-Length': pdfBuffer.length
+        });
+
+        // Send the PDF buffer as the response
+        res.send(pdfBuffer);
+
+        logger.info("PDF Created Successfully");
+        res.status(201).send("Generate PDF query successfull");
+
+    } catch (error) {
+        // Log the error before sending any response
+        logger.error("Error generating PDF:", error.message);
+
+        // If the stream hasn't started, send an error response
+        if (!res.headersSent) {
+            res.status(500).json(
+                new ErrorResponse(
+                    500,
+                    "Generate PDF query failed",
+                    error.message
+                )
+            );
+        } else {
+            logger.error("Stream error after headers sent, cannot send JSON response.");
+        }
+    }
+}
+
 module.exports = {
     generateAllAirfieldsPdf,
     generateSelectedAirfieldsPdf,
     generateAllPilotsPdf,
     generateSelectedPilotsPdf,
     generateAllAircraftsPdf,
-    generateSelectedAircraftsPdf
+    generateSelectedAircraftsPdf,
+    generateAllFlightsPdf,
+    generateSelectedFlightsPdf
 }
